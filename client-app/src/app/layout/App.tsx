98a02dc6c5e6c8573a914384dd371cd59e3e0ca7
MariuszKorotko <mariusz.korotko@wp.pl>
2022-01-18 15:19:32 +0100

    Apply formik into project and datepicker

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,36 +1,41 @@
-import { Container } from 'semantic-ui-react';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import NavBar from './NavBar';
-import { observer } from 'mobx-react-lite';
-import { Route, Switch, useLocation } from 'react-router-dom';
-import HomePage from '../../features/home/HomePage';
-import ActivityForm from '../../features/activities/form/ActivityForm';
-import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import { ToastContainer } from 'react-toastify';
-import TestErrors from '../../features/errors/TestError';
-import NotFound from '../../features/errors/NotFound';
-import ServerError from '../../features/errors/ServerError';
+import React from "react";
+import { Container } from "semantic-ui-react";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import NavBar from "./NavBar";
+import { observer } from "mobx-react-lite";
+import { Route, Switch, useLocation } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import { ToastContainer } from "react-toastify";
+import TestErrors from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
-      <ToastContainer position='bottom-right' hideProgressBar/>
-      <Route exact path='/' component={HomePage}/>
+      <ToastContainer position="bottom-right" hideProgressBar />
+      <Route exact path="/" component={HomePage} />
       <Route
-        path={'/(.+)'}
+        path={"/(.+)"}
         render={() => (
           <>
-            <NavBar/>
-            <Container style={{marginTop: '7em'}}>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
               <Switch>
-                <Route exact path='/activities' component={ActivityDashboard}/>
-                <Route path='/activities/:id' component={ActivityDetails}/>
-                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}/>
-                <Route path='/errors' component={TestErrors}/>
-                <Route path='/server-error' component={ServerError}/>
-                <Route component={NotFound}/>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route path="/errors" component={TestErrors} />
+                <Route path="/server-error" component={ServerError} />
+                <Route component={NotFound} />
               </Switch>
             </Container>
           </>
@@ -40,4 +45,4 @@ function App() {
   );
 }
 
-export default observer (App);
+export default observer(App);
